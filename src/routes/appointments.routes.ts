191825deccfb4802e1a214e7e8f0a1d6c06aa79e
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-02-18 21:32:59 -0300

    trabalhando com dados

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -11,6 +11,7 @@ appointmentsRouter.get('/', (request, response) => {
 
   return response.json(appointments);
 });
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
@@ -26,7 +27,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });

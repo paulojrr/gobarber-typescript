6402c4c7d2aeafcbb63a8c19a483e31de589eab9
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-02-21 07:25:06 -0300

    autenticação

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f54cc7c286a8f43bfb33dcd0265af044',
+    expiresIn: '1d',
+  },
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,13 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
 import usersRouter from './users.routes';
+import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-  return response.json(user);
-});
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
